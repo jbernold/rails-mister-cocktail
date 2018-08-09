a74db0a3309a2d9d45a20babfe1a3e2053e9a4c7
Justin Bernold <jbernold@gmail.com>
2018-08-09 13:17:21 -0400

    validated the fuckness out of the models

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,2 +1,10 @@
 class Dose < ApplicationRecord
+  belongs_to :cocktail
+  belongs_to :ingredient
+
+  validates :description, presence: true
+  validates :cocktail, uniqueness: {scope: :ingredient}
+  validates :ingredient, presence: true
+  validates :cocktail, presence: true
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_09_162413) do
+ActiveRecord::Schema.define(version: 2018_08_09_162414) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,4 +37,6 @@ ActiveRecord::Schema.define(version: 2018_08_09_162413) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "doses", "cocktails"
+  add_foreign_key "doses", "ingredients"
 end
